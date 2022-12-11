1a0f9cd862c70543ae4fe267fba93e82ff2f398e
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-12-11 16:57:36 -0500

    added credit stuff

--- animation.py
+++ animation.py
@@ -1,6 +1,9 @@
-from Scenes import Introduction
+from Scenes import Introduction, ending
 
 
 create_init_circle = Introduction.create_initial_circle()
 
 create_init_circle.construct()
+
+credits = ending.credits()
+credits.construct()
